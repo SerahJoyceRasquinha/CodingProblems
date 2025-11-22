fad5d008c0e73b6a45e37186663161d70e21d14d
Serah Joyce Rasquinha <serahrasquinha@gmail.com>
2025-11-23 00:50:19 +0530

    program 5

---
+++ ConceptBasedProblems/04_Factorial/factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int fact = factorial(a);
+        System.out.println("Result: "+fact);
+    }
+    
+    public static int factorial(int n){
+        int num = n;
+        int fact = 1;
+        for(int i=n; i>0; i--){
+            fact = fact*i;
+        }
+        return fact;
+    }
+}
