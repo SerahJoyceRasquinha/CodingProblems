94d79755d043e70cc2da30065463d78e19199330
Serah Joyce Rasquinha <serahrasquinha@gmail.com>
2025-11-23 13:48:49 +0530

    update

--- ConceptBasedProblems/05_DigitSum/DigitSum.java
+++ ConceptBasedProblems/05_DigitSum/DigitSum.java
@@ -1,22 +1,32 @@
 import java.util.Scanner;
 
-class Main {
+class test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the integer: ");
         String a = sc.next();
         int digitsum = sum_digit(a);
         System.out.println("Result: "+digitsum);
     }
     
     public static int sum_digit(String n){
-        char s;
+        char ch1;
         int digsum = 0;
+        
         for(int i=n.length()-1; i>=0; i--){
-            s = n.charAt(i);
-            digsum += s - '0';  
+            ch1 = n.charAt(i);
+            if((ch1 == '0') || (ch1 == '1') || (ch1 == '2') || (ch1 == '3') || (ch1 == '4') || (ch1 == '5') || (ch1 == '6') || (ch1 == '7') || (ch1 == '8') || (ch1 == '9')){
+                digsum += ch1 - '0'; 
+            }
+            
+            else{
+                    System.out.println("Invalid Integer");
+                    break; 
+                }
         }
-// here we cannot use type casting i.e: (int)s + digsum because the type casting causes that character to be converted to its ASCII integer value. So to convert this to an integer, we are subrtacting a base ASCII value from the given character and then there will be implicit type casting done by the compiler itself (converting char to int)
 
         return digsum;
     }
 }
+
+// here we cannot use type casting i.e: (int)s + digsum because the type casting causes that character to be converted to its ASCII integer value. So to convert this to an integer, we are subrtacting a base ASCII value from the given character and then there will be implicit type casting done by the compiler itself (converting char to int)
